2895a3ea50446764961d53dce2076bf079fb859c
Mitar <mitar.git@tnode.com>
2023-10-21 00:59:23 +0200

    Refactor.

--- service.go
+++ service.go
@@ -404,34 +404,6 @@ func (s *Service[SiteT]) computeEtags() errors.E {
 	return nil
 }
 
-type buildContext struct {
-	Version        string `json:"version,omitempty"`
-	BuildTimestamp string `json:"buildTimestamp,omitempty"`
-	Revision       string `json:"revision,omitempty"`
-}
-
-type siteContext[SiteT hasSite] struct {
-	Site  SiteT         `json:"site"`
-	Build *buildContext `json:"build,omitempty"`
-}
-
-func (s *Service[SiteT]) getSiteContext(site SiteT) siteContext[SiteT] {
-	c := siteContext[SiteT]{
-		Site:  site,
-		Build: nil,
-	}
-
-	if c.Build.Version != "" || s.BuildTimestamp != "" || s.Revision != "" {
-		c.Build = &buildContext{
-			Version:        s.Version,
-			BuildTimestamp: s.BuildTimestamp,
-			Revision:       s.Revision,
-		}
-	}
-
-	return c
-}
-
 func (s *Service[SiteT]) getSite(req *http.Request) (SiteT, errors.E) {
 	if site, ok := s.Sites[req.Host]; req.Host != "" && ok {
 		return site, nil
