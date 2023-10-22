b858b4e5a0687dd0cb586ec4e7eb483f9426e481
Mitar <mitar.git@tnode.com>
2023-10-22 13:29:33 +0200

    More tests.

--- router_test.go
+++ router_test.go
@@ -3,6 +3,7 @@ package waf
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"strings"
 	"testing"
@@ -530,4 +531,143 @@ func TestRouterPathMissing(t *testing.T) {
 
 	_, err = r.path("PathNameMissing", nil, nil, false)
 	assert.ErrorContains(t, err, "route does not exist")
+}
+
+func TestRouterServeHTTP(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		description          string
+		method               string
+		path                 string
+		api                  bool
+		handler              Handler
+		request              *http.Request
+		expectedStatus       int
+		expectedResponseBody string
+		expectedAllowHeader  string
+		expectPanic          string
+	}{
+		{
+			description:          "NotFound handler test",
+			method:               http.MethodGet,
+			path:                 "/",
+			api:                  false,
+			handler:              func(w http.ResponseWriter, req *http.Request, params Params) {},
+			request:              httptest.NewRequest(http.MethodGet, "/notfound", nil),
+			expectedStatus:       http.StatusNotFound,
+			expectedResponseBody: "Not Found\n",
+			expectedAllowHeader:  "",
+			expectPanic:          "",
+		},
+		{
+			description:          "MethodNotAllowed handler test",
+			method:               http.MethodGet,
+			path:                 "/users/:id/posts",
+			api:                  false,
+			handler:              func(w http.ResponseWriter, req *http.Request, params Params) {},
+			request:              httptest.NewRequest(http.MethodPost, "/users/123/posts", nil),
+			expectedStatus:       http.StatusMethodNotAllowed,
+			expectedResponseBody: "Method Not Allowed\n",
+			expectedAllowHeader:  "GET, HEAD",
+			expectPanic:          "",
+		},
+		{
+			description: "Valid non-API route with a GET request",
+			method:      http.MethodGet,
+			path:        "/users/:id/posts",
+			api:         false,
+			handler: func(w http.ResponseWriter, req *http.Request, params Params) {
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write([]byte("Handler for route"))
+			},
+			request:              httptest.NewRequest(http.MethodGet, "/users/123/posts", nil),
+			expectedStatus:       http.StatusOK,
+			expectedResponseBody: "Handler for route",
+			expectedAllowHeader:  "",
+			expectPanic:          "",
+		},
+		{
+			description:          "Valid non-API route with a POST non-API request",
+			method:               http.MethodGet,
+			path:                 "/users/:id/posts",
+			api:                  false,
+			handler:              func(w http.ResponseWriter, req *http.Request, params Params) {},
+			request:              httptest.NewRequest(http.MethodPost, "/users/123/posts", nil),
+			expectedStatus:       http.StatusMethodNotAllowed,
+			expectedResponseBody: "Method Not Allowed\n",
+			expectedAllowHeader:  "GET, HEAD",
+			expectPanic:          "",
+		},
+		{
+			description:          "Valid non-API route with a POST API request",
+			method:               http.MethodGet,
+			path:                 "/users/:id/posts",
+			api:                  false,
+			handler:              func(w http.ResponseWriter, req *http.Request, params Params) {},
+			request:              httptest.NewRequest(http.MethodPost, "/api/users/123/posts", nil),
+			expectedStatus:       http.StatusMethodNotAllowed,
+			expectedResponseBody: "Method Not Allowed\n",
+			expectedAllowHeader:  "",
+			expectPanic:          "",
+		},
+		{
+			description: "Valid API route with a POST request",
+			method:      http.MethodPost,
+			path:        "/users/:id/posts",
+			api:         true,
+			handler: func(w http.ResponseWriter, req *http.Request, params Params) {
+				w.WriteHeader(http.StatusCreated)
+				_, _ = w.Write([]byte("Handler for route"))
+			},
+			request:              httptest.NewRequest(http.MethodPost, "/api/users/123/posts", nil),
+			expectedStatus:       http.StatusCreated,
+			expectedResponseBody: "Handler for route",
+			expectedAllowHeader:  "",
+			expectPanic:          "",
+		},
+		{
+			description: "Handler panics",
+			method:      http.MethodPost,
+			path:        "/users/:id/posts",
+			api:         true,
+			handler: func(w http.ResponseWriter, req *http.Request, params Params) {
+				panic(errors.New("panic error"))
+			},
+			request:              httptest.NewRequest(http.MethodPost, "/api/users/123/posts", nil),
+			expectedStatus:       http.StatusInternalServerError,
+			expectedResponseBody: "",
+			expectedAllowHeader:  "",
+			expectPanic:          "panic error",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.description, func(t *testing.T) {
+			t.Parallel()
+
+			var panicked interface{}
+
+			r := &Router{
+				Panic: func(w http.ResponseWriter, req *http.Request, err interface{}) {
+					panicked = err
+					w.WriteHeader(http.StatusInternalServerError)
+				},
+			}
+			err := r.Handle("PathName", tt.method, tt.path, tt.api, tt.handler)
+			require.NoError(t, err)
+
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, tt.request)
+
+			assert.Equal(t, tt.expectedStatus, w.Code)
+			assert.Equal(t, tt.expectedResponseBody, w.Body.String())
+			assert.Equal(t, tt.expectedAllowHeader, w.Header().Get("Allow"))
+			if tt.expectPanic != "" {
+				assert.Equal(t, tt.expectPanic, fmt.Sprintf("%s", panicked))
+			}
+		})
+	}
 }
