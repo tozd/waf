b75cac3933737c938cfbb2ff862e49afaa7e5c3d
Mitar <mitar.git@tnode.com>
2023-10-24 00:32:48 +0200

    More tests.

--- utils_test.go
+++ utils_test.go
@@ -1,9 +1,13 @@
 package waf
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetHost(t *testing.T) {
@@ -32,4 +36,63 @@ func TestGetHost(t *testing.T) {
 			assert.Equal(t, tt.expected, result)
 		})
 	}
+}
+
+func TestParsePostForm(t *testing.T) {
+	t.Parallel()
+
+	queryString := "key1=value1&key2=value2"
+	postBody := "key3=value3&key4=value4"
+	req := httptest.NewRequest(http.MethodPost, "/example?"+queryString, strings.NewReader(postBody))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	err := parsePostForm(req)
+	assert.NoError(t, err)
+	assert.Nil(t, req.Form)
+
+	require.NotNil(t, req.PostForm)
+	assert.Len(t, req.PostForm, 2)
+	assert.Equal(t, "value3", req.PostForm.Get("key3"))
+	assert.Equal(t, "value4", req.PostForm.Get("key4"))
+}
+
+func TestGetQueryForm(t *testing.T) {
+	t.Parallel()
+
+	queryString := "key1=value1&key2=value2"
+	postBody := "key3=value3&key4=value4"
+	req := httptest.NewRequest(http.MethodPost, "/example?"+queryString, strings.NewReader(postBody))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	queryForm, err := getQueryForm(req)
+	assert.NoError(t, err)
+
+	assert.Len(t, queryForm, 2)
+	assert.Equal(t, "value1", queryForm.Get("key1"))
+	assert.Equal(t, "value2", queryForm.Get("key2"))
+
+	assert.Nil(t, req.Form)
+	assert.Nil(t, req.PostForm)
+
+	err = parsePostForm(req)
+	assert.NoError(t, err)
+	assert.Nil(t, req.Form)
+
+	require.NotNil(t, req.PostForm)
+	assert.Len(t, req.PostForm, 2)
+	assert.Equal(t, "value3", req.PostForm.Get("key3"))
+	assert.Equal(t, "value4", req.PostForm.Get("key4"))
+
+	queryForm, err = getQueryForm(req)
+	assert.NoError(t, err)
+
+	assert.Len(t, queryForm, 2)
+	assert.Equal(t, "value1", queryForm.Get("key1"))
+	assert.Equal(t, "value2", queryForm.Get("key2"))
+
+	assert.Nil(t, req.Form)
+	assert.NotNil(t, req.PostForm)
+	assert.Len(t, req.PostForm, 2)
+	assert.Equal(t, "value3", req.PostForm.Get("key3"))
+	assert.Equal(t, "value4", req.PostForm.Get("key4"))
 }
