4a6e7739af3661cc707e1259dc16802e646fc98d
Mitar <mitar.git@tnode.com>
2023-11-16 11:00:17 +0100

    Documentation.

--- waf.go
+++ waf.go
@@ -1,4 +1,4 @@
-// Package waf provides reusable components of a web application framework.
+// Package waf provides reusable components of a Vue-compatible web application framework.
 //
 // Those components can be combined in a way to create a production ready
 // HTTP/1.1 and HTTP2 application server.
